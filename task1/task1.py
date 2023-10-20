168a586ee0cc13331b9c407f9e2d5780eb5d7eb4
Electrotubbie <steen2033@yandex.ru>
2023-10-21 01:12:34 +0500

    Edit readme. Added dirs for all tasks. Make 1st task.

---
+++ task1/task1.py
@@ -0,0 +1,34 @@
+'''
+Считайте файл согласно вашему варианту и подсчитайте частоту каждого слова в тексте. В результирующем файле выведите полученные данные в порядке убывания:
+word1:freq1
+word2:freq2
+word3:freq3
+...
+wordN:freqN
+'''
+CASE_CHECK = True # учёт регистра (можно сменить на False чтоб не учитывать)
+FILE_NAME = 'task1/text_1_var_37'
+with open(FILE_NAME, 'r') as file:
+    dirty_text = file.read() # условно грязный текст = текст с знаками препинания
+
+# чистка текста
+signs = set(ch for ch in list(dirty_text) if not ch.isalpha()) # поиск множества знаков препинания
+clear_text = dirty_text if CASE_CHECK else dirty_text.lower() # присвоение чистому состояния грязного перед чисткой
+for sign in signs:
+    clear_text = clear_text.replace(sign, ' ') # чистка от знаков препинания путём замены их на пробелы
+clear_text = clear_text.strip() # обрезание лишних пробелов по краям при их наличии
+
+# подсчёт повторяющихся слов
+list_text = clear_text.split(' ') # создание списка слов из текста, отделяя их пробелами
+word_list = set(list_text)
+word_list.discard('') # множество всех возможных слов
+result = [
+    {'word':word, 'freq':list_text.count(word)}
+    for word in word_list
+    ] # подчёт количества повторений слова
+result.sort(key=lambda x: x['freq'], reverse=True)
+print(*[f'{elem["word"]}:{elem["freq"]}' for elem in result], sep='\n')
+
+# запись результата согласно заданию в файл
+with open(f'{FILE_NAME}_answer', 'w') as file:
+    file.writelines([f'{elem["word"]}:{elem["freq"]}' + '\n' for elem in result])
